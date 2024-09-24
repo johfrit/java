b741e74374c3d220058ecc03c22939cc942a9d44
Johfrit Conrad <johfrit2004@gmail.com>
2024-09-24 10:38:23 +0530

    score in float

---
+++ code2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class joh {
+    public static void main(String args[]){
+        Scanner john=new Scanner(System.in);
+        String name=john.nextLine();
+        int score=john.nextInt();
+        john.nextLine();
+        String dept=john.nextLine();
+        float e=(float)score/10;
+        System.out.print(e);
+        
+        
+     
+        
+    }
+}
