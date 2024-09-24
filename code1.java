02a49658a32ee47b17a259be9eb5a6a34bcaf930
Johfrit Conrad <johfrit2004@gmail.com>
2024-09-24 10:15:18 +0530

    name, age basic program

---
+++ code1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class joh {
+    public static void main(String args[]){
+        Scanner john=new Scanner(System.in);
+        String name=john.nextLine();
+        int age=john.nextInt();
+        System.out.print(name+" "+age);
+        
+     
+        
+    }
+}
